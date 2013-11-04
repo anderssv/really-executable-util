77304dbc1b10b5e3b8cfa33678fb570eb8becaa1
Anders Sveen <anders@f12.no>
2013-11-04 22:39:02 +0100

    Updates for OS X and docs

--- bin/buildArchetype.sh
+++ bin/buildArchetype.sh
@@ -3,6 +3,13 @@
 mvn archetype:create-from-project
 cd target/generated-sources/archetype
 # Can we filter this better with the arhcetype plugin?
-rm -rf .eclipse .classpath target bin
-sed -i 's/<mainClass>no\.f12/<mainClass>\$\{groupId\}/g' src/main/resources/archetype-resources/pom.xml
+rm -rf .eclipse .classpath target src/main/resources/arechetype-resources/bin
+cat src/main/resources/archetype-resources/pom.xml | sed 's/<mainClass>no\.f12/<mainClass>\$\{groupId\}/g' > src/main/resources/archetype-resources/pom.xml
 mvn install
+
+echo ""
+echo "Archetype installed!"
+echo ""
+echo "Use with:"
+echo "  mvn archetype:generate -DgroupId=myorg -DartifactId=myapp -Dversion=1.0-SNAPSHOT -DarchetypeGroupId=no.f12 -DarchetypeArtifactId=really-executable-util-archetype"
+echo " "
